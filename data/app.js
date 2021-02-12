6b176ded3e196473c8289f587bf2179ecfaaf3b2
Alexander Narvaez <alexnaravez012@gmail.com>
2021-02-12 12:27:56 -0500

    add drop of filter

--- data/app.js
+++ data/app.js
@@ -1,4 +1,4 @@
-function openLogin(type) {
+function openDropMenus(type) {
     let bodyHeight = document.getElementById('body-height-customers')
     let higthMax = bodyHeight.scrollHeight
     console.log(higthMax + "px !important")
@@ -6,20 +6,17 @@ function openLogin(type) {
     if (type == 'login') {
         document.getElementById("menu-login").setAttribute("style", `height: ${higthMax}px !important`);
         document.getElementById("menu-login").classList.toggle("show");
+        document.getElementById("menu-filter").classList.remove("show");
+    }
+    if (type == 'filter') {
+        document.getElementById("menu-filter").setAttribute("style", `height: ${higthMax}px !important`);
+        document.getElementById("menu-filter").classList.toggle("show");
+        document.getElementById("menu-login").classList.remove("show");
     }
     if (type == 'sesion-started') {
         document.getElementById("menu-sesion-started").setAttribute("style", `height: ${higthMax}px !important`);
         document.getElementById("menu-sesion-started").classList.toggle("show");
     }
-
-
-}
-
-function openOrders(type) {
-    let bodyHeight = document.getElementById('body-height-customers')
-    let higthMax = bodyHeight.scrollHeight
-    console.log(higthMax + "px !important")
-
     if (type == 'orders') {
         document.getElementById("menu-orders").setAttribute("style", `height: ${higthMax}px !important`);
         document.getElementById("menu-orders").classList.toggle("show");
