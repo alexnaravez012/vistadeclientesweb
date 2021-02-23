6d5b5626f791c3cb5b0de843f243e148378720c7
Alexander Narvaez <alexnaravez012@gmail.com>
2021-02-23 11:13:57 -0500

    finish corrections

--- data/app.js
+++ data/app.js
@@ -12,18 +12,16 @@ function openDropMenus(type) {
         document.getElementById("menu-filter").setAttribute("style", `height: ${higthMax}px !important`);
         document.getElementById("menu-filter").classList.toggle("show");
         document.getElementById("menu-login").classList.remove("show");
+        document.getElementById("menu-sesion-started").classList.remove("show");
     }
     if (type == 'sesion-started') {
         document.getElementById("menu-sesion-started").setAttribute("style", `height: ${higthMax}px !important`);
         document.getElementById("menu-sesion-started").classList.toggle("show");
+        document.getElementById("menu-filter").classList.remove("show");
     }
     if (type == 'orders') {
         document.getElementById("menu-orders").setAttribute("style", `height: ${higthMax}px !important`);
         document.getElementById("menu-orders").classList.toggle("show");
-    }
-    if (type == 'body-show') {
         document.getElementById("menu-filter").classList.remove("show");
-        document.getElementById("menu-login").classList.remove("show");
-        console.log("body");
     }
 }
